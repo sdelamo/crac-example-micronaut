9262eb7c24fdc69411cecaa804eec51711ce51be
Sergio del Amo <sergio.delamo@softamo.com>
2022-06-21 17:13:20 +0200

    remove dependencies and don’t return publisher

--- src/main/java/example/HelloController.java
+++ src/main/java/example/HelloController.java
@@ -3,18 +3,13 @@ package example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.validation.Validated;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
-
 import javax.validation.constraints.NotBlank;
 
-@Controller("/")
-@Validated
+@Controller
 public class HelloController {
 
     @Get(uri = "/hello/{name}", produces = MediaType.TEXT_PLAIN)
-    public Publisher<String> hello(@NotBlank String name) {
-        return Mono.just("Hello " + name + "!");
+    public String hello(@NotBlank String name) {
+        return "Hello " + name + "!";
     }
 }
